1f78465d8c8bfa9ba14ae392d2f3bd08d24db117
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2018-12-04 22:53:35 -0500

    Added more empty functions to read SMB2/3 implementation

--- modules/smbfs/smbfs_vfsops.cc
+++ modules/smbfs/smbfs_vfsops.cc
@@ -10,6 +10,21 @@
 #include <osv/mount.h>
 #include <osv/dentry.h>
 
+static int
+smbfs_mount(struct mount *mp, const char *dev, int flags, const void *data)
+{
+}
+
+static int
+smbfs_unmount(struct mount *mp, int flags)
+{
+}
+
+// For the following let's rely on operations on individual files
+#define smbfs_sync    ((vfsop_sync_t)vfs_nullop)
+#define smbfs_vget    ((vfsop_vget_t)vfs_nullop)
+#define smbfs_statfs  ((vfsop_statfs_t)vfs_nullop)
+
 struct vfsops smbfs_vfsops = {
     smbfs_mount,	/* mount */
     smbfs_unmount,	/* unmount */
