f09447abcfe2d23fc70b0d7270aa5fb21ac8b308
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2018-12-06 12:59:47 -0500

    Added initial implementation of getattr call

--- modules/smbfs/smbfs_vnops.cc
+++ modules/smbfs/smbfs_vnops.cc
@@ -38,6 +38,25 @@ static inline struct smb2dir *get_dir_handle(struct vnode *node)
     return static_cast<struct smb2dir *>(node->v_data);
 }
 
+static const char *get_node_name(struct vnode *node)
+{
+    if (LIST_EMPTY(&node->v_names) == 1) {
+        return nullptr;
+    }
+
+    return LIST_FIRST(&node->v_names)->d_path;
+}
+
+static inline struct timespec to_timespec(uint64_t sec, uint64_t nsec)
+{
+    struct timespec t;
+
+    t.tv_sec = sec;
+    t.tv_nsec = nsec;
+
+    return t;
+}
+
 static int smbfs_open(struct file *fp)
 {
     struct vnode *vp = file_dentry(fp)->d_vnode;
@@ -135,26 +154,66 @@ static int smbfs_close(struct vnode *vp, struct file *fp)
 
 //
 // This function reads as much data as requested per uio
-static int smbfs_read(struct vnode *vnode, struct file *fp, struct uio *uio, int ioflag)
+static int smbfs_read(struct vnode *vp, struct file *fp, struct uio *uio, int ioflag)
 {
 }
 
 //
 // This functions reads directory information (dentries) based on information in memory
 // under rofs->dir_entries table
-static int smbfs_readdir(struct vnode *vnode, struct file *fp, struct dirent *dir)
+static int smbfs_readdir(struct vnode *vp, struct file *fp, struct dirent *dir)
 {
 }
 
 //
 // This functions looks up directory entry based on the directory information stored in memory
 // under rofs->dir_entries table
-static int smbfs_lookup(struct vnode *vnode, char *name, struct vnode **vpp)
+static int smbfs_lookup(struct vnode *vp, char *name, struct vnode **vpp)
 {
 }
 
-static int smbfs_getattr(struct vnode *vnode, struct vattr *attr)
+static int smbfs_getattr(struct vnode *vp, struct vattr *attr)
 {
+    int err_no;
+    auto smb2 = get_smb2_context(vp, err_no);
+    if (err_no) {
+        return err_no;
+    }
+
+    auto path = get_node_name(vp);
+    if (!path) {
+        return ENOENT;
+    }
+
+    // Get the file infos.
+    struct smb2_stat_64 st;
+    int ret = smb2_stat(smb2, path, &st);
+    if (ret) {
+        return -ret;
+    }
+
+    //uint32_t smb2_type;
+
+    //uint64_t type = st.nfs_mode & S_IFMT;
+    //uint64_t mode = st.nfs_mode & ~S_IFMT;
+
+    // Copy the file infos.
+    //attr->va_mask    =;
+    //TODO: attr->va_type    = IFTOVT(type);
+    //TODO: attr->va_mode    = mode;
+    attr->va_nlink   = st.smb2_nlink;
+    //attr->va_uid     = st.smb2_uid;
+    //attr->va_gid     = st.smb2_gid;
+    //attr->va_fsid    = st.smb2_dev; // FIXME: not sure about this one
+    attr->va_nodeid  = st.smb2_ino;
+    attr->va_atime   = to_timespec(st.smb2_atime, st.smb2_atime_nsec);
+    attr->va_mtime   = to_timespec(st.smb2_mtime, st.smb2_mtime_nsec);
+    attr->va_ctime   = to_timespec(st.smb2_ctime, st.smb2_ctime_nsec);
+    //attr->va_rdev    = st.smb2_rdev;
+    //attr->va_nblocks = st.smb2_blocks;
+    attr->va_size    = st.smb2_size;
+
+    return 0;
 }
 
 #define smbfs_write       ((vnop_write_t)vop_erofs)
