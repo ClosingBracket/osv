4d0a8cc8ebcce90e7dc65d7058c1b3291d664ac1
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-10-18 08:00:52 -0400

    More tweaks

--- scripts/tests/test_app.py
+++ scripts/tests/test_app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from testing import *
 import argparse
 import subprocess
---
+++ scripts/tests/test_selected_apps.sh
@@ -0,0 +1,54 @@
+# For java apps it wold be nice to test them with java.so and /usr/bin/java as well as verious versions of Java - 8, 11, 12, 13
+# Possibly build and test with rofs and zfs
+#----
+#* Run and chec output for some text
+osv.golang-example                                 golang-example                                     1.0.0           2019-09-11 22:21     Ubuntu 19.04
+osv.golang-pie-example                             golang-pie-example                                 1.0.0           2019-09-11 22:22     Ubuntu 19.04
+osv.graalvm-example                                graalvm-example                                    1.0.0           2019-09-11 22:24     Ubuntu 19.04
+osv.lua-hello-from-host                            Lua Hello                                          5.3.3           2019-09-12 07:35     Ubuntu 19.04
+osv.rust-example                                   rust-example                                       1.0.0           2019-09-11 22:19     Ubuntu 19.04
+osv.stream                                         Stream                                             1.0.0           2019-09-12 07:37     Ubuntu 19.04
+
+osv.python2-from-host                              Python 2.7                                         2.7.16          2019-09-11 21:21     Ubuntu 19.04
+osv.python3-from-host                              Python 3.7                                         3.7.3           2019-09-11 21:22     Ubuntu 19.04
+
+# http with curl -------
+osv.golang-httpserver                              golang-httpserver                                  1.0.0           2019-09-11 22:22     Ubuntu 19.04
+osv.golang-pie-httpserver                          golang-pie-httpserver                              1.0.0           2019-09-11 22:23     Ubuntu 19.04
+osv.graalvm-httpserver                             graalvm-httpserver                                 1.0.0           2019-09-11 22:29     Ubuntu 19.04
+osv.jetty                                          Jetty                                              9.4.19.v20190610 2019-09-11 20:59     Ubuntu 19.04
+osv.lighttpd                                       Lighttpd                                           1.4.54          2019-09-11 21:21     Ubuntu 19.04
+osv.nginx                                          Nginx                                              1.2.12          2019-09-12 07:35     Ubuntu 19.04
+osv.rust-httpserver                                rust-httpserver                                    1.0.0           2019-09-11 22:21     Ubuntu 19.04
+osv.spring-boot-example                            Spring Boot Demo                                   2.1.8.RELEASE   2019-09-12 07:37     Ubuntu 19.04
+tomcat
+osv.vertx                                          Vertx.io example                                   3.8.1           2019-09-12 07:38     Ubuntu 19.04
+
+# Multiple versions of node
+osv.node-express-example                           node-express-example                               1.0.0           2019-09-11 22:34     Ubuntu 19.04
+osv.node-from-host                                 Node JS                                            v10.15.2        2019-09-11 21:24     Ubuntu 19.04
+
+# With tester
+osv.iperf3                                         Iperf 3                                            3.6             2019-09-11 21:25     Ubuntu 19.04
+osv.apache-derby                                   Apache Derby DB                                    10.14.1.0       2019-09-11 21:00     Ubuntu 19.04
+osv.apache-kafka                                   Apache Kafka                                       2.3.0           2019-09-12 07:34     Ubuntu 19.04
+osv.elasticsearch                                  Elasticsearch                                      7.3.1           2019-09-11 20:59     Ubuntu 19.04
+osv.ffmpeg                                         Ffmpeg                                             4.2             2019-09-12 07:35     Ubuntu 19.04
+osv.graalvm-netty-plot                             graalvm-netty-plot                                 1.0.0           2019-09-11 22:33     Ubuntu 19.04
+osv.mysql                                          MySQL                                              5.6.45          2019-09-11 21:32     Ubuntu 19.04
+osv.netperf                                        Netperf                                            2.7.0           2019-09-11 21:26     Ubuntu 19.04
+osv.redis-memonly                                  Redis                                              3.2.8           2019-09-11 21:27     Ubuntu 19.04
+osv.memcached                                      Memcached                                          1.4.21          2019-09-11 21:27     Ubuntu 19.04
+
+# Requires input
+osv.cli                                            OSv Command Line                                   0.53.0-1c1c1602 2019-09-11 20:29     Ubuntu 19.04
+
+osv.httpserver-api-tests                           OSv httpserver API tests                           0.53.0-1c1c1602 2019-09-11 22:19     Ubuntu 19.04
+osv.httpserver-api                                 OSv httpserver with APIs (backend)                 0.53.0-1c1c1602 2019-09-11 20:28     Ubuntu 19.04
+osv.unit-tests                                     OSv Unit Tests                                     0.53.0-1c1c1602 2019-09-11 22:18     Ubuntu 19.04
+
+# --- runtimes, utility
+osv.openjdk11-zulu                                 Zulu Open JDK 11                                   11.0.4 2019-07-16 LTS 2019-09-11 20:33     Ubuntu 19.04
+osv.openjdk8-zulu-compact3-with-java-beans         Zulu Open JDK 8 compact profile 3 with java.beans  1.8.0_222       2019-09-11 20:33     Ubuntu 19.04
+osv.run-go                                         Run Golang wrapper                                 0.53.0-1c1c1602 2019-09-11 20:29     Ubuntu 19.04
+osv.run-java                                       Run Java wrapper                                   0.53.0-1c1c1602 2019-09-11 20:29     Ubuntu 19.04
