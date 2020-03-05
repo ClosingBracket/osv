c517ed09471357956c674485de9c8bbdb6cdcf6d
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-03-05 08:28:55 -0500

    Fixed cirp publish.sh

--- .travis/cirp/publish.sh
+++ .travis/cirp/publish.sh
@@ -9,16 +9,21 @@ if [ ! -z "$TRAVIS_TEST_RESULT" ] && [ "$TRAVIS_TEST_RESULT" != "0" ]; then
   exit 0
 fi
 
-if [ "$#" != "1" ]; then
-  echo "Error: No arguments provided. Please specify a directory containing artifacts as the first argument."
+if [ "$#" != "2" ]; then
+  echo "Error: No arguments provided. Please specify a directory containing artifacts as the first argument and the release name."
   exit 1
 fi
 
 ARTIFACTS_DIR="$1"
+OSV_VERSION="$2"
 
 . .travis/cirp/install.sh
 
 ci-release-publisher publish --latest-release \
                              --latest-release-prerelease \
                              --latest-release-check-event-type push \
+                             --numbered-release \
+                             --numbered-release-keep-count 10 \
+                             --numbered-release-prerelease \
+                             --numbered-release-name "$OSV_VERSION" \
                              "$ARTIFACTS_DIR"
