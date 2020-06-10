29d7a056867d053754a5bfe179c00437772f85cc
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-06-10 10:46:35 -0400

    Another script

--- scripts/generate_version_script2.sh
+++ scripts/generate_version_script2.sh
@@ -1,1 +1,1 @@
-nm -uC --format posix apps/redis-memonly/redis-server | cut -d "@" -f 1 | cut -f 1 | awk '// { printf("    %s;\n", $0) }'
+nm -uC --format posix apps/rust-pie-httpserver/target/release/httpserver | cut -d "@" -f 1 | cut -f 1 | awk '// { printf("    %s;\n", $0) }'
