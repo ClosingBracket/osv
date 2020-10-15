388a8f59edf779e9a786d30b3b93eeb47ac2a2fd
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-10-15 10:02:13 -0400

    Most tls tests working

--- tests/libtls.cc
+++ tests/libtls.cc
@@ -5,23 +5,26 @@
  * BSD license as described in the LICENSE file in the top-level directory.
  */
 
+#include <cstdio>
+
 // ex2 and ex3 have different attributes defined here than in tst-tls.cc
 // That's on purpose, no matter what TLS access model is used in the end
 // the same part of memory should be accessed.
 __thread int ex1 = 321;
 __thread int ex2 __attribute__ ((tls_model ("initial-exec"))) = 432;
-__thread int ex3 = 765;
+//__thread int ex3 = 765;
 
 extern __thread int v1;
-extern __thread int v5;
+//extern __thread int v5;
 
 void external_library()
 {
     // ex1 and ex3 get accessed by _tls_get_addr()
     ex1++;
     ex2++;
-    ex3++;
+    //ex3++;
     // These 2 below get handled by get _tls_get_addr() function in core/elf.cc
     v1++;
-    v5++;
+    //v5++;
+    printf("-> In external_library\n");
 }
